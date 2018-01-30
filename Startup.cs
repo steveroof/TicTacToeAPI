6304a203dabbacccb743f92c9502a4c6f3f4a73c
Roof <stephen.roof@bracketglobal.com>
2018-01-30 13:31:56 -0500

    add cors logic

--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Cors;
 using TicTacToeAPI.Models;
 
 namespace TicTacToeAPI
 {
     public class Startup
-    {       
+    {
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<TicTacToeContext>(opt => opt.UseInMemoryDatabase("TicTacToeList"));
             services.AddMvc();
+            services.AddCors(options => //https://docs.microsoft.com/en-us/aspnet/core/security/cors
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin();
+                    });
+            });
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            // Shows UseCors with named policy.
+            app.UseCors("AllowAllOrigins");
             app.UseMvc();
         }
     }
